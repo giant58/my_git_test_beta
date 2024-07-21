e36fb9deb166c9154c2c1b00bf44cad0f8da1415
Giant Su <giantsu@gmail.com>
2024-07-21 11:15:37 +0800

    first

---
+++ git_basic.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 21 11:02:10 2024
+
+@author: ca_su
+"""
+
+'''
+執行: (在windows的search列打) git bash
+		○ git --version    # 顯示目前安裝git版本
+	• 開始讓某個資料夾有版本控制功能
+		○ cd <該資料夾目錄>     # 可以直接拖拉該資料夾到bash視窗
+		○ git init    # 開始版本控制
+	• 設定個人資訊(追蹤用，不必和git帳號一致)
+		○ git config --global user.name "xxx"   # 姓名
+		○ git config --global user.email "xxx@gmail.com"  # email
+		○ git config --list  # 顯示設定; (最後按'q'結束)
+	• 基本指令
+        ○ git status   # 檢查目前目錄是否有更動
+'''
